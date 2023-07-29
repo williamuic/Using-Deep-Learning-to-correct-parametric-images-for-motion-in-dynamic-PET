e78f2e977d4ac8d292808d0f8ac4dbed8b1b9d52
William <ucaphwe@ucl.ac.uk>
2023-07-29 19:04:40 +0100

    Correct mistakes for evaluation

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,3 +1,5 @@
+# evaluation_metrics.py
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -5,38 +7,7 @@ from skimage.metrics import structural_similarity as ssim, peak_signal_noise_rat
 from sklearn.metrics import mean_squared_error as mse
 import os
 
-
 class EvaluationMetrics:
-    """
-    A class used to calculate evaluation metrics for comparing images.
-
-    Attributes
-    ----------
-    folder_path : str
-        Path to the folder containing the images.
-    num_images : int
-        Number of images to be evaluated.
-    pairs : list
-        List of pairs of image names to compare.
-    channels : list
-        List of image channels.
-    y_start, y_end, x_start, x_end : int
-        Indices to crop the images.
-
-    Methods
-    -------
-    plot_image(img, title):
-        Plots a given image.
-    view_images(index):
-        Loads and displays images of a certain index.
-    calculate_metrics(real_img, fake_img):
-        Calculates metrics for a pair of real and fake images.
-    calculate_average_metrics():
-        Calculates and prints average metrics over a number of images.
-    evaluate_one_pair(pair, index):
-        Evaluates a pair of images of a certain index.
-    """
-
     def __init__(self, folder_path, num_images, pairs, channels, y_start=64, y_end=192, x_start=0, x_end=256):
         self.folder_path = folder_path
         self.num_images = num_images
@@ -48,23 +19,13 @@ class EvaluationMetrics:
         self.x_end = x_end
 
     def plot_image(self, img, title):
-        """
-        Plots the given image.
-
-        Parameters:
-        img: numpy array
-            The image data to plot.
-        title: str
-            The title for the plot.
-        """
-        fig, axs = plt.subplots(1, img.shape[0], figsize=(12, 12))
+        fig, axs = plt.subplots(1, img.shape[0], figsize=(12,12))
         for i in range(img.shape[0]):
             im = axs[i].imshow(img[i])
             axs[i].set_title(f'{title} - Channel {i+1}')
             axs[i].axis('off')
 
-            # create an axes on the right side of axs[i]
-            # The width of cax will be 5% of axs[i] and the padding between cax and axs[i] will be fixed at 0.05 inch
+            # create an axes on the right side of axs[i]. The width of cax will be 5% of axs[i] and the padding between cax and axs[i] will be fixed at 0.05 inch.
             divider = make_axes_locatable(axs[i])
             cax = divider.append_axes("right", size="5%", pad=0.05)
 
@@ -72,13 +33,6 @@ class EvaluationMetrics:
         plt.show()
 
     def view_images(self, index):
-        """
-        Load and display images of a certain index.
-
-        Parameters:
-        index: int
-            The index of the images to display.
-        """
         for name in self.pairs:
             file_path = os.path.join(self.folder_path, f"{name}{index}.npy")
             if os.path.exists(file_path):
@@ -88,18 +42,6 @@ class EvaluationMetrics:
                 print(f"The file {file_path} does not exist.")
 
     def calculate_metrics(self, real_img, fake_img):
-        """
-        Calculates various metrics for a pair of real and fake images.
-
-        Parameters:
-        real_img: numpy array
-            The real image data.
-        fake_img: numpy array
-            The fake image data.
-
-        Returns:
-        A dictionary containing RMSE, NMSE, NRMSE, SSIM, and PSNR for each channel.
-        """
         real_img = real_img.squeeze()
         fake_img = fake_img.squeeze()
 
@@ -108,8 +50,7 @@ class EvaluationMetrics:
             "Vd": {}
         }
 
-        # If the image only has one channel
-        if real_img.ndim == 2:
+        if real_img.ndim == 2:  # If the image only has one channel
             real_channel = real_img[self.y_start:self.y_end, self.x_start:self.x_end]
             fake_channel = fake_img[self.y_start:self.y_end, self.x_start:self.x_end]
 
@@ -148,58 +89,77 @@ class EvaluationMetrics:
                     "SSIM": ssim_value,
                     "PSNR": psnr_value,
                 }
+
         return metrics
 
+
     def calculate_average_metrics(self):
-        """
-        Calculates and prints average metrics over a number of images.
-        """
-        metrics_values = {key: {metric: [] for metric in ["RMSE", "NMSE", "NRMSE", "SSIM", "PSNR"]}
-                          for key in self.channels}
+        metrics_totals = {
+            pair: {
+                "Ki": {"RMSE": 0, "NMSE": 0, "NRMSE": 0, "SSIM": 0, "PSNR": 0},
+                "Vd": {"RMSE": 0, "NMSE": 0, "NRMSE": 0, "SSIM": 0, "PSNR": 0}
+            } for pair in self.pairs
+        }
 
         for i in range(self.num_images):
             for pair in self.pairs:
-                real_file_path = os.path.join(self.folder_path, f"{pair[0]}{i}.npy")
-                fake_file_path = os.path.join(self.folder_path, f"{pair[1]}{i}.npy")
+                real_img_path = os.path.join(self.folder_path, f"{pair[0]}{i}.npy")
+                fake_img_path = os.path.join(self.folder_path, f"{pair[1]}{i}.npy")
+                if os.path.exists(real_img_path) and os.path.exists(fake_img_path):
+                    real_img = np.load(real_img_path)
+                    fake_img = np.load(fake_img_path)
 
-                if os.path.exists(real_file_path) and os.path.exists(fake_file_path):
-                    real_img = np.load(real_file_path)
-                    fake_img = np.load(fake_file_path)
+                    # Add an extra dimension to single-channel images
+                    if real_img.ndim == 2:
+                        real_img = real_img[np.newaxis, :]
+                    if fake_img.ndim == 2:
+                        fake_img = fake_img[np.newaxis, :]
 
                     metrics = self.calculate_metrics(real_img, fake_img)
+                    for channel_name in self.channels:
+                        for metric_name, metric_value in metrics[channel_name].items():
+                            metrics_totals[pair][channel_name][metric_name] += metric_value
+                else:
+                    print(f"Missing one of the files {real_img_path}, {fake_img_path}")
+                    return
 
-                    for channel in self.channels:
-                        for metric in ["RMSE", "NMSE", "NRMSE", "SSIM", "PSNR"]:
-                            metrics_values[channel][metric].append(metrics[channel][metric])
-
-        for channel in self.channels:
-            print(f"\n{channel} Channel Metrics:\n")
-            for metric in ["RMSE", "NMSE", "NRMSE", "SSIM", "PSNR"]:
-                print(f"Avg {metric}: {np.mean(metrics_values[channel][metric])}")
+        # Calculate and print average metrics
+        for pair in self.pairs:
+            for channel_name in self.channels:
+                print(f"\nAverage metrics for {pair[0]} and {pair[1]} - Channel {channel_name}:")
+                for metric_name in metrics_totals[pair][channel_name]:
+                    avg_metric_value = metrics_totals[pair][channel_name][metric_name] / self.num_images
+                    print(f"{metric_name}: {avg_metric_value:.5f}")
 
     def evaluate_one_pair(self, pair, index):
-        """
-        Evaluates a pair of images of a certain index.
+        real_img_path = os.path.join(self.folder_path, f"{pair[0]}{index}.npy")
+        fake_img_path = os.path.join(self.folder_path, f"{pair[1]}{index}.npy")
+        if os.path.exists(real_img_path) and os.path.exists(fake_img_path):
+            real_img = np.load(real_img_path)
+            fake_img = np.load(fake_img_path)
 
-        Parameters:
-        pair: tuple
-            The pair of images to evaluate.
-        index: int
-            The index of the images to evaluate.
-        """
-        real_file_path = os.path.join(self.folder_path, f"{pair[0]}{index}.npy")
-        fake_file_path = os.path.join(self.folder_path, f"{pair[1]}{index}.npy")
+            # Add an extra dimension to single-channel images
+            if real_img.ndim == 2:
+                real_img = real_img[np.newaxis, :]
+            if fake_img.ndim == 2:
+                fake_img = fake_img[np.newaxis, :]
 
-        if os.path.exists(real_file_path) and os.path.exists(fake_file_path):
-            real_img = np.load(real_file_path)
-            fake_img = np.load(fake_file_path)
-
-            self.view_images(index)
             metrics = self.calculate_metrics(real_img, fake_img)
+            for channel_name in self.channels:
+                print(f"\nMetrics for {pair[0]} and {pair[1]} - Channel {channel_name}:")
+                for metric_name, metric_value in metrics[channel_name].items():
+                    print(f"{metric_name}: {metric_value:.5f}")
+            
+            for channel_index, channel_name in enumerate(self.channels):
+                fig, axs = plt.subplots(1, 2, figsize=(10, 10))
+                im_real = axs[0].imshow(real_img[0, channel_index, self.y_start:self.y_end, self.x_start:self.x_end])
+                axs[0].set_title(f'{pair[0]} Image - Channel {channel_name}')
+                fig.colorbar(im_real, ax=axs[0], fraction=0.046, pad=0.04)
 
-            for channel in self.channels:
-                print(f"\n{channel} Channel Metrics:\n")
-                for metric, value in metrics[channel].items():
-                    print(f"{metric}: {value}")
+                im_fake = axs[1].imshow(fake_img[0, channel_index, self.y_start:self.y_end, self.x_start:self.x_end])
+                axs[1].set_title(f'{pair[1]} Image - Channel {channel_name}')
+                fig.colorbar(im_fake, ax=axs[1], fraction=0.046, pad=0.04)
+
+                plt.show()
         else:
-            print(f"The files {real_file_path} and/or {fake_file_path} do not exist.")
+            print(f"One or both of the files {real_img_path}, {fake_img_path} do not exist.")
